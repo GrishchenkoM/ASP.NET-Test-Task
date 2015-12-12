611b9b7a65f32dc21da88bf18e5d2c0b7286562e
Michael <grishchenko.m@yandex.ru>
2015-12-12 21:15:43 +0200

    another garbage

--- ListGeneration.Tests/ListGenerationTests.cs
+++ ListGeneration.Tests/ListGenerationTests.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
-namespace ListGeneration.Tests
+namespace ListGenerationTests
 {
+    [TestFixture]
     public class ListGenerationTests
     {
+        [Test]
+        public void GenerateRandomNumbersList()
+        {
+            var list = ListGeneration.GetList();
+        }
     }
 }
+
+
