77e9864db72c09723ed9946784de9e86297d2077
Michael <grishchenko.m@yandex.ru>
2015-12-14 15:13:05 +0200

    new HomeController with Session and paging realization

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ListGeneration;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        
+        public ActionResult Index(int pageNum = 0)
+        {
+            if (Session["List"] == null)
+                Session["List"] = new RandomList(StandartQuantity);
+
+            if (pageNum < 0)
+                pageNum = 0;
+
+            var model = new ListGenerationModel
+            {
+                Array = (((IEnumerable<int>)((RandomList)Session["List"]).GetList).Skip(PageSize * pageNum).Take(PageSize).ToList())
+            };
+            ViewBag.CurrentPageNum = pageNum;
+            return View(model);
+        }
+
+        private const int StandartQuantity = 10000;
+        private const int PageSize = 250;
+    }
+}
