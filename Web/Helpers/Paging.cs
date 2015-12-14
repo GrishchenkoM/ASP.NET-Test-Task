8231bd56dbe8f4d83c1629a60abe982bfc4b2ba2
Michael <grishchenko.m@yandex.ru>
2015-12-14 15:18:21 +0200

    create Paging helper

---
+++ Web/Helpers/Paging.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Web.Helpers
+{
+    public static class Paging
+    {
+        public static MvcHtmlString PagingNavigator(this HtmlHelper helper, int pageNum)
+        {
+            var sb = new StringBuilder();
+            sb.Append(helper.ActionLink("<<<<<<<", "Index", new {pageNum = pageNum - 1}));
+            sb.Append(helper.ActionLink(">>>>>>>", "Index", new {pageNum = pageNum + 1}));
+            return MvcHtmlString.Create(sb.ToString());
+        }
+    }
+}
