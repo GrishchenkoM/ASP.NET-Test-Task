07751b3e6313185c8f2fa05bd86f10a489ec1bf2
Michael <grishchenko.m@yandex.ru>
2015-12-14 15:14:49 +0200

    new Model: ListGenerationModel with List<int> Array property

---
+++ Web/Models/ListGenerationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class ListGenerationModel
+    {
+        public List<int> Array { get; set; }
+    }
+}
