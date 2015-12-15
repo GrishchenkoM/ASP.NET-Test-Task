e54cabe1c314d3c1c84360fc5f3104420daf76d6
Michael <grishchenko.m@yandex.ru>
2015-12-15 13:38:47 +0200

    create HttpPost Index action and GetContent for Ajax realization whicj doesn't work. Code refactoring

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using ListGeneration;
 using Web.Models;
@@ -10,9 +9,6 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-        
         public ActionResult Index(int pageNum = 0)
         {
             if (Session["List"] == null)
@@ -21,13 +17,57 @@ namespace Web.Controllers
             if (pageNum < 0)
                 pageNum = 0;
 
-            var model = new ListGenerationModel
-            {
-                Array = (((IEnumerable<int>)((RandomList)Session["List"]).GetList).Skip(PageSize * pageNum).Take(PageSize).ToList())
-            };
+            var model = new ListGenerationModel();
+            FillModel(model, pageNum);
+
             ViewBag.CurrentPageNum = pageNum;
             ViewBag.PagesCount = (int)Math.Ceiling((double)StandartQuantity / PageSize);
+
+            // Was created for Ajax realization. Not working
+            if (Request.IsAjaxRequest())
+                return PartialView("IndexContent", model);
+            
             return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index()
+        {
+            Session["List"] = new RandomList(StandartQuantity);
+
+            var model = new ListGenerationModel();
+            FillModel(model);
+
+            ViewBag.CurrentPageNum = 0;
+            ViewBag.PagesCount = (int) Math.Ceiling((double) StandartQuantity/PageSize);
+            
+            return View(model);
+        }
+
+        // Was created for Ajax realization. Not working
+        public ActionResult GetContent(int pageNum = 0)
+        {
+            if (Session["List"] == null)
+                Session["List"] = new RandomList(StandartQuantity);
+
+            if (pageNum < 0)
+                pageNum = 0;
+
+            var model = new ListGenerationModel();
+            FillModel(model, pageNum);
+
+            ViewBag.CurrentPageNum = pageNum;
+            ViewBag.PagesCount = (int)Math.Ceiling((double)StandartQuantity / PageSize);
+            return View("IndexContent", model);
+        }
+
+        private void FillModel(ListGenerationModel model, int pageNum = 0)
+        {
+            model.Array =
+                (((IEnumerable<int>) ((RandomList) Session["List"]).GetList)
+                    .Skip(PageSize*pageNum)
+                    .Take(PageSize)
+                    .ToList());
         }
 
         private const int StandartQuantity = 10000;
